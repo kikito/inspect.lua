66ffbd5ed1205c005172a16b9db490aaa797a8ed
kikito <kikito@gmail.com>
2012-10-30 00:05:39 +0100

    bumped version to 1.2.0

--- inspect.lua
+++ inspect.lua
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------------------------------------------------
--- inspect.lua - v1.1.1 (2011-01)
+-- inspect.lua - v1.2.0 (2012-10)
 -- Enrique García Cota - enrique.garcia.cota [AT] gmail [DOT] com
 -- human-readable representations of tables.
 -- inspired by http://lua-users.org/wiki/TableSerialization
