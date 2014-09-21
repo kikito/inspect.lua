8df8cd8b332d4c2962d6a0cf6ebbcc207dd93500
kikito <kikito@gmail.com>
2014-09-21 11:25:29 +0200

    upped version. Fixes #17

--- inspect.lua
+++ inspect.lua
@@ -1,5 +1,5 @@
 local inspect ={
-  _VERSION = 'inspect.lua 2.0.0',
+  _VERSION = 'inspect.lua 3.0.0',
   _URL     = 'http://github.com/kikito/inspect.lua',
   _DESCRIPTION = 'human-readable representations of tables',
   _LICENSE = [[
